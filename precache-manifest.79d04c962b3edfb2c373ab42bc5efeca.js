886be1900e07ba2a3a2dd028dad70d0e9499f8c8
Ashikur Rahman <ashikurjhalak@gmail.com>
2019-11-01 18:34:06 +0600

    Updates

---
+++ precache-manifest.79d04c962b3edfb2c373ab42bc5efeca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0708b64842d1635dd4838daa87a8c6",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "69b853de457fe50830b8",
+    "url": "/myportfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "19763c506ceff6464891",
+    "url": "/myportfolio/static/css/main.92f01031.chunk.css"
+  },
+  {
+    "revision": "69b853de457fe50830b8",
+    "url": "/myportfolio/static/js/2.93ec0b6b.chunk.js"
+  },
+  {
+    "revision": "19763c506ceff6464891",
+    "url": "/myportfolio/static/js/main.27df8bd9.chunk.js"
+  },
+  {
+    "revision": "f6da0988cc920d9604bb",
+    "url": "/myportfolio/static/js/runtime-main.4c1d368d.js"
+  }
+]);
